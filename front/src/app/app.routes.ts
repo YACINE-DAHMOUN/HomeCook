2c9b9c03e1c85b4ec74ae0f452d402d56a17a355
YACINEDAHMOUN <yacine.dahmoun15@gmail.com>
2025-06-15 01:45:56 +0200

    Add routing paths for login, login-form, user-pages, and home components

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,17 @@
+import { User } from './models/user.interface';
+import { LoginFormComponent } from './pages/login-form/login-form.component';
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { UserPagesComponent } from './pages/user-pages/user-pages.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: 'login', component: LoginComponent },
+  {path: 'login-form', component: LoginFormComponent },
+  {path: 'user-pages', component: UserPagesComponent },
+  {path: 'home', component: HomeComponent }
+
+];
+
+
