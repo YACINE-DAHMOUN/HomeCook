7284c3525fa9bfbe3c829898494a95df6f3e262f
YACINEDAHMOUN <yacine.dahmoun15@gmail.com>
2025-07-07 20:58:10 +0200

    Rename login-form route and import to register-form component

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { User } from './models/user.interface';
-import { LoginFormComponent } from './pages/login-form/login-form.component';
+import { LoginFormComponent } from './pages/register-form/register-form.component';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { UserPagesComponent } from './pages/user-pages/user-pages.component';
@@ -8,7 +8,7 @@ import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent },
-  {path: 'login-form', component: LoginFormComponent },
+  {path: 'register-form', component: LoginFormComponent },
   {path: 'user-pages', component: UserPagesComponent },
   {path: 'home', component: HomeComponent }
 
